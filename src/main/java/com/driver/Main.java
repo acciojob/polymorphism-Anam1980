29ad3352553970e7f7c13500ad21de3a2aa42f72
Your Name <you@example.com>
2023-08-10 19:47:34 +0530

    Add Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return 1;
+        }
+        public int product(int x, int y, int z) {
+            return 2;
+        }
+        public double product(double x, double y) {
+            return 3.0;
+        }
+    }
+
+    public static  void main(String[]args){
+        Product p = new Product();
+        //task 2
+        System.out.println(p.product(0,1));
+
+        //task 3
+        System.out.println(p.product(1,1,1));
+
+        //task 4
+        System.out.println(p.product(44.5,5.5));
+
+    }
 }
